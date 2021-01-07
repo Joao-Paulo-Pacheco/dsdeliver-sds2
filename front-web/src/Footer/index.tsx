eb747e947690868ad33d0b16cbe989af5bda7bf1
João Paulo Pacheco <joaopaulo.pacheco@outlook.com.br>
2021-01-07 16:36:10 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css'
+
+import {ReactComponent as IconYoutube} from './youtube.svg'; 
+import {ReactComponent as IconLinkedin} from './linkedin.svg';
+import {ReactComponent as IconInstagram} from './instagram.svg';  
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <IconYoutube/>
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                    <IconLinkedin/>
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/">
+                    <IconInstagram/>
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
